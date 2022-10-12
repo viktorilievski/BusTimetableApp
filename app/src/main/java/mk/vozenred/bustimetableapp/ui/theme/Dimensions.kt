f25f07d4c9f641043c2e8b165f5db81ad96291dc
Viktor Ilievski <36957657+viktorilievski@users.noreply.github.com>
2022-10-12 11:08:49 +0200

    Designed new relation cards.

--- app/src/main/java/mk/vozenred/bustimetableapp/ui/theme/Dimensions.kt
+++ app/src/main/java/mk/vozenred/bustimetableapp/ui/theme/Dimensions.kt
@@ -1,15 +1,12 @@
 package mk.vozenred.bustimetableapp.ui.theme
 
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 val SMALL_PADDING = 4.dp
 val MEDIUM_PADDING = 8.dp
 val LARGE_PADDING = 16.dp
-
-val SMALL_MARGIN = 4.dp
-val MEDIUM_MARGIN = 8.dp
-val LARGE_MARGIN = 16.dp
 
 val POINT_DROPDOWN_HEIGHT = 60.dp
 val POINT_ROW_ITEM_HEIGHT = 50.dp
@@ -17,3 +14,10 @@ val POINT_ROW_ITEM_HEIGHT = 50.dp
 val DRAWABLE_SIZE_BIG = 100.dp
 val DRAWABLE_SIZE_MEDIUM = 60.dp
 val DRAWABLE_SIZE_SMALL = 30.dp
+
+val TEXT_SIZE_LARGE = 25.sp
+val TEXT_SIZE_MEDIUM = 18.sp
+val TEXT_SIZE_SMALL = 12.sp
+
+val CARD_CORNER_RADIUS = 40.dp
+
